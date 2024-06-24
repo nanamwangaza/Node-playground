93b22adf8f29e8c8989cc0fc2e6ce46e5583fa0b
nanamwangaza <nanamwangaza60@gmail.com>
2024-06-24 20:41:28 +0300

    added moe models

--- vidly/index.js
+++ vidly/index.js
@@ -3,11 +3,15 @@ const config = require('config');
 const app = express();
 const genres = require('./routes/genres');
 const customers =require('./routes/customers');
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 const mongoose=require('mongoose');
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 mongoose.connect("mongodb://localhost/vidly")
 .then(()=> console.log("Connected to mongob....."))
